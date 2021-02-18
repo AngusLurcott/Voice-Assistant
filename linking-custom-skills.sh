ab29032abfd921f4e7ae661b92f9fca73be8c982
Anson Gurung <gurunga4@cardiff.ac.uk>
2021-02-18 01:11:58 +0000

    Added argument in script to install requirements of custom skills

--- linking-custom-skills.sh
+++ linking-custom-skills.sh
@@ -8,9 +8,29 @@ CUSTOM_SKILL_DIR="custom_skills"
 
 TOP=$(pwd -L)
 
-if [ -d "$SKILLS_DIR" -a -d "$CUSTOM_SKILL_DIR" ]; then
+function install_requirements_from_subdirs() {
     echo "Installing Requirements for Custom Skills"
+    for skill_dir in "$TOP/$CUSTOM_SKILL_DIR"/*/; 
+    do
+        echo "Currently in $skill_dir"
+        if [ -f "$skill_dir/requirements.txt" ]; then
+            mycroft-pip install -r "$skill_dir/requirements.txt"
+        fi
+    done
+}
 
+for var in "$@" ; do
+    # Check for options
+    if [[ $var == '-ir' || $var == '--install-requirements' ]] ; then
+        if install_requirements_from_subdirs; then
+            exit 0
+        else
+            exit 1
+        fi
+    fi
+done
+
+if [ -d "$SKILLS_DIR" -a -d "$CUSTOM_SKILL_DIR" ]; then
     echo "Removing any broken soft links..."
     find "$SKILLS_DIR" -xtype l -exec rm '{}' \;
 
