ad765c49636692e9df615dc434811dbd94488ea2
Anson Gurung <gurunga4@cardiff.ac.uk>
2021-02-21 16:24:11 +0000

    Adding files and changes from forslund MR

--- mycroft/skills/__main__.py
+++ mycroft/skills/__main__.py
@@ -40,6 +40,7 @@ from mycroft.util.lang import set_active_lang
 from mycroft.util.log import LOG
 from mycroft.util.process_utils import ProcessStatus, StatusCallbackMap
 from .core import FallbackSkill
+from .api import SkillApi
 from .event_scheduler import EventScheduler
 from .intent_service import IntentService
 from .skill_manager import SkillManager
@@ -211,6 +212,7 @@ def main(alive_hook=on_alive, started_hook=on_started, ready_hook=on_ready,
                                   on_stopping=stopping_hook)
     status = ProcessStatus('skills', bus, callbacks)
 
+    SkillApi.connect_bus(bus)
     skill_manager = _initialize_skill_manager(bus, watchdog)
 
     status.set_started()
