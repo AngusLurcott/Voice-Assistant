f77a3e686a3f97855f78ed6a68a8572ee6ab7d90
Angus Lurcott <lurcotta@cardiff.ac.uk>
2021-03-14 20:38:46 +0000

    User can now play 5 radio stations

--- custom_skills/radio-skill/__init__.py
+++ custom_skills/radio-skill/__init__.py
@@ -11,24 +11,10 @@ from mycroft.skills import MycroftSkill, skill_api_method
 from mycroft.skills.core import MycroftSkill, intent_handler, skill_api_method
 #api skill get skill
 from mycroft.skills.api import SkillApi
-
+# Import audio skill
 from mycroft.skills.audioservice import AudioService
 
-
-# try:
-#     from mycroft.skills.audioservice import AudioService
-# except:
-#     from mycroft.util import play_mp3
-#     AudioService = None
-
-
-
-__author__ = 'nmoore'
-
-
-LOGGER = getLogger(__name__)
-
-
+# Reference https://github.com/normandmickey/skill-internet-radio
 class InternetRadioSkill(MycroftSkill):
     def __init__(self):
         super(InternetRadioSkill, self).__init__(name="InternetRadioSkill")
@@ -47,12 +33,60 @@ class InternetRadioSkill(MycroftSkill):
     @intent_file_handler('bbcradio1.intent')
     def handle_bbcradio1(self, message):
         self.stop()
-        self.speak_dialog('Playing.the.radio')
+        self.speak_dialog("'Playing BBC Radio 1'")
         time.sleep(4)
 
 
 
-        self.audio_service.play('https://redirect.viglink.com/?format=go&jsonp=vglnk_161541752509712&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km41vroy01000ad7000DLbfwrh5yh&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8&ref=https%3A%2F%2Fwww.google.com%2F&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8')                               
+        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575420247533&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8")      
+
+    @intent_file_handler('bbcradio2.intent')
+    def handle_bbcradio2(self, message):
+        self.stop()
+        self.speak_dialog('Playing BBC Radio 2')
+        time.sleep(4)
+
+
+
+        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575421242234&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_two.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_two.m3u8")   
+
+
+    @intent_file_handler('bbcradio3.intent')
+    def handle_bbcradio3(self, message):
+        self.stop()
+        self.speak_dialog('Playing BBC Radio 3')
+        time.sleep(4)
+
+
+
+        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575422526535&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_three.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_three.m3u8")  
+
+
+
+    @intent_file_handler('bbcradio4.intent')
+    def handle_bbcradio4(self, message):
+        self.stop()
+        self.speak_dialog('Playing BBC Radio 4')
+        time.sleep(4)
+
+
+
+        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575415916532&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_four_extra.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_four_extra.m3u8")  
+
+
+
+
+    @intent_file_handler('bbcradio5live.intent')
+    def handle_bbcradio5live(self, message):
+        self.stop()
+        self.speak_dialog('Playing BBC Radio 5 Live')
+        time.sleep(4)
+
+
+
+        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575424224036&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_five_live.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_five_live.m3u8")  
+
+
 
             
 
