ca5356405e8a5821b60e64f4259122c632aa4a49
Angus Lurcott <lurcotta@cardiff.ac.uk>
2021-03-15 20:53:29 +0000

    Fixed issue with playing wrong station

--- custom_skills/radio-skill/README.md
+++ custom_skills/radio-skill/README.md
@@ -1,33 +1,5 @@
-## internet radio
-Play internet radio station.
+## Radio skill
+Use this skill to play BBC radio stations
 
 ## Description 
-Play internet radio stations using Mycroft.  
-Manage your station URLs at home.mycroft.ai -> Skills.
-
-## Examples 
-* "internet radio"
-* "web radio"
-* "play some music"
-* "rock radio"
-* "play rock radio"
-* "listen to rock radio"
-* "country radio"
-* "play country radio"
-* "listen to country radio"
-* "classical radio"
-* "play classical radio"
-* "listen to classical radio"
-* "jazz radio"
-* "play jazz radio"
-* "listen to jazz radio"
-* "top 40 radio"
-* "play top 40 radio"
-* "listen to top 40 radio"
-* "christmas radio"
-* "play christmas radio"
-* "listen to christmas radio"
-
-## Credits 
-Norman Moore
-
+Say "radio" or "listen to the radio" to invoke the skill. You will then be asked which station you would like to play
--- custom_skills/radio-skill/__init__.py
+++ custom_skills/radio-skill/__init__.py
@@ -6,7 +6,7 @@ import random
 from mycroft.util.log import getLogger
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.skills import MycroftSkill, skill_api_method
-
+from mycroft.audio import wait_while_speaking
 #skill api
 from mycroft.skills.core import MycroftSkill, intent_handler, skill_api_method
 #api skill get skill
@@ -23,77 +23,72 @@ class InternetRadioSkill(MycroftSkill):
 
 
     def initialize(self):
-
-
-        
         self.audio_service = AudioService(self.bus)
 
-        
 
-    @intent_file_handler('bbcradio1.intent')
-    def handle_bbcradio1(self, message):
-        self.stop()
-        self.speak_dialog("'Playing BBC Radio 1'")
-        time.sleep(4)
+    @intent_file_handler('radio.intent')
+    def handle_radio(self, message):
+        self.stop() #Stop incase radio is currently playing
 
+        self.speak_dialog('What BBC radio station would you like to play? One, two, three, four, or five live')
 
-
-        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575420247533&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8")      
-
-    @intent_file_handler('bbcradio2.intent')
-    def handle_bbcradio2(self, message):
-        self.stop()
-        self.speak_dialog('Playing BBC Radio 2')
-        time.sleep(4)
+        time.sleep(10)
 
 
 
-        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575421242234&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_two.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_two.m3u8")   
+        try:
+            chosen_station = self.get_response().lower()
+
+            if chosen_station == '1' or chosen_station == 'one':
+
+                self.speak_dialog('Playing BBC Radio 1')
+                time.sleep(4)
+                self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575420247533&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_one.m3u8")
 
 
-    @intent_file_handler('bbcradio3.intent')
-    def handle_bbcradio3(self, message):
-        self.stop()
-        self.speak_dialog('Playing BBC Radio 3')
-        time.sleep(4)
+            elif chosen_station == '2' or chosen_station == 'two':
 
-
-
-        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575422526535&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_three.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_three.m3u8")  
-
-
-
-    @intent_file_handler('bbcradio4.intent')
-    def handle_bbcradio4(self, message):
-        self.stop()
-        self.speak_dialog('Playing BBC Radio 4')
-        time.sleep(4)
+                self.speak_dialog('Playing BBC Radio 2')
+                time.sleep(4)
 
 
 
-        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575415916532&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_four_extra.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_four_extra.m3u8")  
+                self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575421242234&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_two.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_two.m3u8")
+
+            elif chosen_station == '3' or chosen_station == 'three':
+
+                self.speak_dialog('Playing BBC Radio 3')
+                time.sleep(4)
 
 
 
-
-    @intent_file_handler('bbcradio5live.intent')
-    def handle_bbcradio5live(self, message):
-        self.stop()
-        self.speak_dialog('Playing BBC Radio 5 Live')
-        time.sleep(4)
-
-
-
-        self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575424224036&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_five_live.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_five_live.m3u8")  
-
-
+                self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575422526535&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_three.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_three.m3u8")
 
             
+            elif chosen_station == '4' or chosen_station == 'four':      
+                self.speak_dialog('Playing BBC Radio 4')
+                time.sleep(4)
 
-            
 
 
-    
+                self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575415916532&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_four_extra.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_four_extra.m3u8")
+
+            elif chosen_station == '5' or chosen_station == 'five' or chosen_station == '5 live' or chosen_station == 'five live' or chosen_station == 'live':
+
+                self.speak_dialog('Playing BBC Radio 5 Live')
+                time.sleep(4)
+
+
+
+                self.audio_service.play("https://redirect.viglink.com/?format=go&jsonp=vglnk_161575424224036&key=8ace3fc8b4830d32ead5520650c6ce6e&libId=km9k2eff01000ad7000DLu95eo64a3cwo&loc=https%3A%2F%2Fwww.hifiwigwam.com%2Fforum%2Ftopic%2F127134-high-quality-320kbps-streams-for-all-bbc-radio-stations%2F&v=1&out=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_five_live.m3u8&title=High%20Quality%20320kbps%20streams%20for%20all%20BBC%20radio%20stations%20-%202%20Channel%20-%20HiFi%20WigWam&txt=http%3A%2F%2Fa.files.bbci.co.uk%2Fmedia%2Flive%2Fmanifesto%2Faudio%2Fsimulcast%2Fhls%2Fuk%2Fsbr_high%2Fak%2Fbbc_radio_five_live.m3u8")
+
+            else:
+                self.speak_dialog('Sorry I did not quite catch that')
+
+
+        except:
+            self.speak_dialog('Please try again')
+
 
     @intent_file_handler('stopradio.intent')
     def handle_stop(self, message):
@@ -101,12 +96,7 @@ class InternetRadioSkill(MycroftSkill):
         self.speak_dialog('Stopping.the.radio')
 
     def stop(self):
-        if self.audio_service:
            self.audio_service.stop()
-        else:
-            if self.process and self.process.poll() is None:
-               self.process.terminate()
-               self.process.wait()
 
 def create_skill():
     return InternetRadioSkill()
